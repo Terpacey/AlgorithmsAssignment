e5ed09cdb326d70253a9a447a80688290c1736a4
Hariharan Nair <69513762+Terpacey@users.noreply.github.com>
2024-02-13 19:50:17 -0500

    Class and node init

---
+++ ConsoleApplication1/Assignment1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+    ProductData data;
+    Node* next;
+        
+    Node()
+    {
+        this->next = NULL;
+    }
+
+    void InsertNode()
+    {
+        Node newNode = Node();
+
+    }
+
+};
+
+class ProductData
+{
+    public:
+        int productID;
+        float productPrice;
+        string productName, productCategory;
+
+        ProductData()
+        {
+            this->productID = 0;
+            this->productPrice = 0.00f;
+            this->productName = "Default Name";
+            this->productCategory = "Default Name";
+        }
+
+        void SetID(int id)
+        {
+            this->productID = id;
+        }
+            void SetPrice(float price)
+        {
+            this->productPrice = price;
+        }
+        void SetName(string name)
+        {
+            this->productName = name;
+        }
+        void SetCategory(string category)
+        {
+            this->productCategory = category;
+        }
+
+        int GetID()
+        {
+            return this->productID;
+        }
+        float GetPrice()
+        {
+            return this->productPrice;
+        }
+        string GetName()
+        {
+            return this->productName;
+        }
+        string GetCategory()
+        {
+            return this->productCategory;
+        }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
